da2ad422889a7e184859c70f227d8ba9c48cf0d3
louis <louislapassat@gmail.com>
2020-07-15 19:30:14 +0200

    Changed the global path and init of london in depth pubs

--- function/data_loader.py
+++ function/data_loader.py
@@ -16,3 +16,22 @@ def data_loader(path='datasets_full/', city='London'):
              for dataset in os.listdir(full_path)]
         )
     )
+
+
+def load_pubs_data(global_path: str) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Treat the data from  https://data.london.gov.uk/dataset/pubs-clubs-restaurants-takeaways-borough
+    :param global_path: str, global path to the data
+    :return: (pd.DataFrame, pd.DataFrame), (number of pubs in the borough, pubs employment in the borough)
+    """
+    # Load data
+    pubs = pd.read_csv(global_path + 'london_data_pubs.csv')
+    pubs_employment = pd.read_csv(global_path + 'london_data_pubs_employment.csv')
+
+    # Treat the data
+    for df in [pubs, pubs_employment]:
+        df.set_index('area', inplace=True)
+        for col in df.columns:
+            df[col] = df[col].apply(lambda x: int(x.replace(',', ''))).astype(int)
+
+    return pubs, pubs_employment
